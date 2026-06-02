8fe35fd7269d0f5c35f402c3f0f299b5b8e98e76
Gabriela Burgel <gabiiburgel@gmail.com>
2026-06-02 11:22:41 -0300

    atributos e gets da classe Transformador

--- src/Transformador.java
+++ src/Transformador.java
@@ -1,2 +1,37 @@
 public class Transformador {
+    private double temperatura;
+    private int idade;
+    private double carga;
+    private double tensao;
+    private boolean riscoCritico;
+
+    public Transformador(double temperatura,
+                         int idade,
+                         double carga,
+                         double tensao,
+                         boolean riscoCritico){
+
+        this.temperatura = temperatura;
+        this.idade = idade;
+        this.carga = carga;
+        this.tensao = tensao;
+        this.riscoCritico = riscoCritico;
+
+    }
+
+    public double getTemperatura(){
+        return temperatura;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public double getCarga(){
+        return carga;
+    }
+    public double getTensao(){
+        return tensao;
+    }
+    public boolean isRiscoCritico(){
+        return riscoCritico;
+    }
 }
