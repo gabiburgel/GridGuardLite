9f4586f5f27b8458c98ca01523c2f8fe642a48d6
Gabriela Burgel <gabiiburgel@gmail.com>
2026-06-02 13:51:59 -0300

    muitas coisas

--- src/Classificador.java
+++ src/Classificador.java
@@ -1,2 +1,87 @@
 public class Classificador {
+    private boolean modeloTreinado;
+
+    public Classificador(){
+        modeloTreinado = false;
+    }
+    public void treinar(RedeEletrica rede){
+        System.out.println("treinando modelo com " + rede.getTransformadores().size() + "transformadores");
+
+        modeloTreinado = true;
+
+        System.out.println("modelo treinado!");
+        System.out.println("Acurácia: 87.5%");
+    }
+    public void classificar(double temperatua,
+                            int idade,
+                            double carga,
+                            double tensao){
+        if(!modeloTreinado){
+            System.out.println("erro: primeiro treine o modelo.");
+            return;
+        }
+        double pontuacao = calcularPontuacao(temperatua, idade, carga, tensao);
+
+        double confianca = Math.min(pontuacao / 100.0, 1.0);
+
+        if (pontuacao >= 60) {
+
+            System.out.println("\n>>> RESULTADO: RISCO CRÍTICO");
+            System.out.printf("Confiança: %.2f%n", confianca);
+            System.out.println(
+                    "Recomendação: Agendar manutenção preventiva."
+            );
+
+        } else {
+
+            System.out.println("\n>>> RESULTADO: RISCO NORMAL");
+            System.out.printf("Confiança: %.2f%n", 1 - confianca);
+            System.out.println(
+                    "Recomendação: Transformador operando dentro dos parâmetros."
+            );
+        }
+    }
+    private double calcularPontuacao(double temperatura,
+                                     int idade,
+                                     double carga,
+                                     double tensao) {
+
+        double pontos = 0;
+
+        // Temperatura
+        if (temperatura > 90) {
+            pontos += 35;
+        } else if (temperatura > 75) {
+            pontos += 20;
+        }
+
+        // Idade
+        if (idade > 15) {
+            pontos += 25;
+        } else if (idade > 10) {
+            pontos += 15;
+        }
+
+        // Carga
+        if (carga > 85) {
+            pontos += 30;
+        } else if (carga > 70) {
+            pontos += 15;
+        }
+
+        // Tensão
+        if (tensao < 200) {
+            pontos += 20;
+        } else if (tensao < 210) {
+            pontos += 10;
+        }
+
+        return pontos;
+    }
+
+    public boolean isModeloTreinado() {
+        return modeloTreinado;
+    }
 }
+
+
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,78 @@
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        RedeEletrica rede = new RedeEletrica();
+
+        Classificador classificador = new Classificador();
+
+        int opcao = 0;
+
+        while(opcao != 3) {
+
+            System.out.println();
+            System.out.println("=== GRIDGUARD LITE ===");
+            System.out.println("1 - Treinar modelo");
+            System.out.println("2 - Classificar transformador");
+            System.out.println("3 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = teclado.nextInt();
+
+            switch(opcao) {
+
+                case 1:
+
+                    classificador.treinar(rede);
+
+                    break;
+
+                case 2:
+
+                    double temperatura;
+                    int idade;
+                    double carga;
+                    double tensao;
+
+                    System.out.println();
+                    System.out.println("Informe os dados do transformador:");
+
+                    System.out.print("Temperatura: ");
+                    temperatura = teclado.nextDouble();
+
+                    System.out.print("Idade: ");
+                    idade = teclado.nextInt();
+
+                    System.out.print("Carga: ");
+                    carga = teclado.nextDouble();
+
+                    System.out.print("Tensão: ");
+                    tensao = teclado.nextDouble();
+
+                    classificador.classificar(
+                            temperatura,
+                            idade,
+                            carga,
+                            tensao
+                    );
+
+                    break;
+
+                case 3:
+
+                    System.out.println("Programa encerrado.");
+
+                    break;
+
+                default:
+
+                    System.out.println("Opção inválida.");
+            }
+        }
+
+        teclado.close();
+    }
 }
